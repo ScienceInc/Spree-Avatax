69ace7697f60d0054a895d01ed0c392fc3ed775d
BryceFrye <bcfrye19@gmail.com>
2014-04-29 18:16:10 -0700

    avatax.yml can now interpret erb

--- config/initializers/load_avatax.rb
+++ config/initializers/load_avatax.rb
@@ -1,3 +1,3 @@
 require 'ostruct'
-raw_config = File.read(::Rails.root.to_s + "/config/avatax.yml")
+raw_config = ERB.new(File.read(::Rails.root.to_s + "/config/avatax.yml")).result
 AvataxConfig = OpenStruct.new(YAML.load(raw_config)[Rails.env])
